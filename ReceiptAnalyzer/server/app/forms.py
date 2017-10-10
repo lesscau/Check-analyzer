d777cefa9f8aaaccb032589c72dcf05ebe870d48
vkrich <vladislav.krichevsky@mail.ru>
2017-10-10 20:08:42 +0300

    Add form and some views

--- ReceiptAnalyzer/server/app/forms.py
+++ ReceiptAnalyzer/server/app/forms.py
@@ -4,9 +4,28 @@ from wtforms import validators
 
 class LoginForm(Form):
     username = StringField('Login:', [validators.DataRequired()])
-    password = StringField('Password:',
-                        [validators.DataRequired()])
+    password = StringField('Password:',  [validators.DataRequired()])
 
     def __init__(self, *args, **kwargs):
         kwargs['csrf_enabled'] = False
         Form.__init__(self, *args, **kwargs)
+
+
+    def edit_login(request):
+    Form = LoginForm(request.POST, username)
+
+    if request.POST and Form.validate():
+        Form.populate_obj(username)
+        username.save()
+        return redirect('/username')
+
+    return render('edit.html', form=form, username=username)
+
+
+class RegisterForm(Form):
+      name = StringField('NickName', [validators.DataRequired()])
+
+
+
+
+
