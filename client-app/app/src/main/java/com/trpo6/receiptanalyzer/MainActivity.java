cab7132567b981c4bea69a9165cba5590a1cc658
Olga <lesscau@gmail.com>
2017-10-11 14:36:02 +0300

    Fixes qr-code scanning

--- client-app/app/src/main/java/com/trpo6/receiptanalyzer/MainActivity.java
+++ client-app/app/src/main/java/com/trpo6/receiptanalyzer/MainActivity.java
@@ -1,18 +1,12 @@
 package com.trpo6.receiptanalyzer;
 
 import android.content.Intent;
-import android.graphics.Camera;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-import com.google.zxing.Result;
-
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
-
-public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,35 +18,10 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
         Intent intent = new Intent("productlist");
         startActivity(intent);
     }
-    private ZXingScannerView mScannerView;
 
-      public void QrScanner(View view){
-          //Intent intent = new Intent (android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-         // startActivityForResult(intent, 0);
-        mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view<br />
-        setContentView(mScannerView);
-          mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.<br />
-        mScannerView.startCamera();         // Start camera<br />
-    }
-   @Override
-    public void onPause() {
-            super.onPause();
-            mScannerView.stopCamera();   // Stop camera on pause<br />
-    }
-
-
-   @Override
-    public void handleResult(Result rawResult) {
-        // Do something with the result here</p>
-     Log.e("handler", rawResult.getText()); // Prints scan results<br />
-        Log.e("handler", rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode)</p>
-       // show the scanner result into dialog box.<br />
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setTitle("Scan Result");
-                builder.setMessage(rawResult.getText());
-                AlertDialog alert1 = builder.create();
-                alert1.show();
-       // If you would like to resume scanning, call this method below:<br />
-        // mScannerView.resumeCameraPreview(this);<br />
+    // Переход к активности сканирования QR-кода с чека
+    public void scanQRcode(View view){
+        Intent intent = new Intent("QRscanner");
+        startActivity(intent);
     }
 }
