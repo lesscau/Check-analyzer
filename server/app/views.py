f5e4e55d804c08aa78a446b8ca56153ba23a3fb5
d@N0n <dan0na@ya.ru>
2017-10-12 08:31:21 +0300

    Fixed local import in views.py

--- server/app/views.py
+++ server/app/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import render_template, flash, redirect
 from app import app
-from forms import LoginForm
+from .forms import LoginForm
 
 @app.route('/login')
 def login():
