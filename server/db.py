6640d140f6708e764f47b2ebdc5846f6a9e3a4af
d@N0n <dan0na@ya.ru>
2017-10-19 18:34:27 +0300

    Fixed db.py indentation

--- server/db.py
+++ server/db.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 from flask_migrate import init, migrate, upgrade, downgrade
-# import argparse
 from app import app
 import sys, datetime
 
@@ -8,24 +7,24 @@ import sys, datetime
 # Always check generated migrations manually!!!
 
 def db_init():
-	with app.app_context():
-		init()
+    with app.app_context():
+        init()
 
 def db_migrate():
-	now = datetime.datetime.now()
-	with app.app_context():
-		migrate(message="{}_{}_{}_{}_{}".format(now.day, now.month, now.year, now.hour, now.minute))
+    now = datetime.datetime.now()
+    with app.app_context():
+        migrate(message="{}_{}_{}_{}_{}".format(now.day, now.month, now.year, now.hour, now.minute))
 
 def db_upgrade():
-	with app.app_context():
-		upgrade()
+    with app.app_context():
+        upgrade()
 
 def db_downgrade():
-	with app.app_context():
-		downgrade()
+    with app.app_context():
+        downgrade()
 
 def help_message():
-	print("""Usage: python migrate.py [init|migrate|upgrade|downgrade|help]
+    print("""Usage: python migrate.py [init|migrate|upgrade|downgrade|help]
 
 COMMANDS:
 help\t\tshow this help message and exit.
@@ -35,17 +34,17 @@ upgrade\t\tupgrades the database to last revision.
 downgrade\tdowngrades the database by 1 revision.""")
 
 if __name__ == '__main__':
-	if len(sys.argv) > 1:
-		if sys.argv[1] == "init":
-			db_init()
-		elif sys.argv[1] == "migrate":
-			db_migrate()
-		elif sys.argv[1] == "upgrade":
-			db_upgrade()
-		elif sys.argv[1] == "downgrade":
-			db_downgrade()
-		else:
-			print("Unknown command\n")
-			help_message()
-	else:
-		help_message()
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "init":
+            db_init()
+        elif sys.argv[1] == "migrate":
+            db_migrate()
+        elif sys.argv[1] == "upgrade":
+            db_upgrade()
+        elif sys.argv[1] == "downgrade":
+            db_downgrade()
+        else:
+            print("Unknown command\n")
+            help_message()
+    else:
+        help_message()
