b7f833f849305e604ce02f8c03967a9782aa9453
d@N0n <dan0na@ya.ru>
2017-10-19 17:39:12 +0300

    Change migrations from sqlalchemy-migrate to flask-migrate

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
 from config import basedir
 
 app = Flask(__name__)
@@ -10,5 +11,7 @@ db = SQLAlchemy(app)
 
 lm = LoginManager()
 lm.init_app(app)
+
+migrate = Migrate(app, db)
 
 from app import views, models
