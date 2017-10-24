809b1d2376cc6c48cde378875604599931a41c97
d@N0n <dan0na@ya.ru>
2017-10-24 19:48:40 +0300

    Implemented base Users REST without auth and error checking

--- server/app/views.py
+++ server/app/views.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from flask import render_template, request, jsonify
-from flask_restful import Resource
+from flask_restful import Resource, reqparse, fields, marshal
 import json
 
-from app import app, api, models
+from app import app, api, db, models
 from .forms import LoginForm
 from .FtsRequest import FtsRequest
 
@@ -11,8 +11,8 @@ APIv1 = "/api/v1.0"
 
 @app.route(APIv1 + '/login')
 def login():
-    form = LoginForm()
-    return render_template('login.json', form = form)
+	form = LoginForm()
+	return render_template('login.json', form = form)
 
 # Password from json not used because it doesn't exist in current User model
 @app.route(APIv1 + '/receipt')
@@ -31,23 +31,77 @@ def getReceipt():
 	receipt = FtsRequest().getReceipt(fn, fd, fp, user.phone, user.fts_key)
 	return jsonify(receipt)
 
+users_fields = {
+    'id': fields.Integer,
+    'username': fields.String,
+    'url': fields.Url('user')
+}
+
+user_fields = {
+    'id': fields.Integer,
+    'username': fields.String,
+    'phone': fields.String,
+    'url': fields.Url('user')
+}
+
 class UserList(Resource):
+	def __init__(self):
+		self.reqparse = reqparse.RequestParser()
+		self.reqparse.add_argument('username', type = str, required = True,
+			help = 'No username provided', location = 'json')
+		self.reqparse.add_argument('password', type = str, required = True,
+			help = 'No password provided', location = 'json')
+		self.reqparse.add_argument('phone', type = str, required = True,
+			help = 'No phone provided', location = 'json')
+		self.reqparse.add_argument('fts_key', type = int, required = True,
+			help = 'No ftskey provided', location = 'json')
+		super(UserList, self).__init__()
+
 	def get(self):
-		pass
+		users = models.User.query.all()
+		return { 'users': marshal(users, users_fields) }
 
 	def post(self):
-		pass
+		args = self.reqparse.parse_args()
+		user = models.User(
+			username = args['username'],
+			password = args['password'],
+			phone = args['phone'],
+			fts_key = args['fts_key'])
+		db.session.add(user)
+		db.session.commit()
+		return { 'user': marshal(user, user_fields) }
 
 class Users(Resource):
+	def __init__(self):
+		self.reqparse = reqparse.RequestParser()
+		self.reqparse.add_argument('username', type = str, location = 'json')
+		self.reqparse.add_argument('password', type = str, location = 'json')
+		self.reqparse.add_argument('phone', type = str, location = 'json')
+		self.reqparse.add_argument('fts_key', type = int, location = 'json')
+		super(Users, self).__init__()
+
 	def get(self, id):
-		pass
+		user = models.User.query.get(id)
+		return { 'user': marshal(user, user_fields) }
 
 	def put(self, id):
-		pass
+		args = self.reqparse.parse_args()
+		user = models.User.query.get(id)
+		for key, value in args.items():
+			if value is not None:
+				setattr(user, key, value)
+		print(user.fts_key)
+		db.session.commit()
+		return { 'user': marshal(user, user_fields) }
 
 class UserInfo(Resource):
 	def get(self):
 		pass
 
+	def put(self):
+		pass
+
 api.add_resource(UserList, APIv1 + '/users', endpoint = 'users')
 api.add_resource(Users, APIv1 + '/users/<int:id>', endpoint = 'user')
+api.add_resource(UserInfo, APIv1 + '/users/me', endpoint = 'userme')
