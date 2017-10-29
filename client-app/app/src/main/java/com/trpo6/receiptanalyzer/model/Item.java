5e3c81e7c9a9e35728035524259483a88b84f93a
LukaBrasi <scretch235@mail.ru>
2017-10-29 14:04:08 +0300

    Documentation for Item.java

--- client-app/app/src/main/java/com/trpo6/receiptanalyzer/model/Item.java
+++ client-app/app/src/main/java/com/trpo6/receiptanalyzer/model/Item.java
@@ -26,41 +26,49 @@ public class Item {
     @Expose
     private float price;
 
+    /**Инициализурет поля {@link Item#name}, {@link Item#selectedCount}, {@link Item#quantity}, {@link Item#price}*/
     public Item(String name, int cnt, float price){
         this.name = name;
         this.selectedCount = 0;
         this.quantity = cnt;
         this.price = price;
     }
-
+    /**Возвращает наименование продукта*/
     public String getName() {
         return name;
     }
 
+    /**Устанавливает наименование продукта*/
     public void setName(String name) {
         this.name = name;
     }
 
+    /**Возвращает количество продуктов, выбранных пользователем*/
     public int getSelectedCount() {
         return selectedCount;
     }
 
+    /**Устанавливает количество продуктов, выбранных пользователем*/
     public void setSelectedCount(int selectedCount) {
         this.selectedCount = selectedCount;
     }
 
+    /**Возвращает цену единицы продукта*/
     public float getPrice() {
         return price;
     }
 
+    /**Устанавливает цену единицы продукта*/
     public void setPrice(float price) {
         this.price = price;
     }
 
+    /**Возвращает количество единиц купленного продукта*/
     public int getQuantity() {
         return quantity;
     }
 
+    /**Устанавливает количество единиц купленного продукта*/
     public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
