ad9d8205581af17f8fd3d3e506164302d6396f4c
LukaBrasi <scretch235@mail.ru>
2017-10-29 15:23:19 +0300

    Documentation for RegActivity.java

--- client-app/app/src/main/java/com/trpo6/receiptanalyzer/activity/RegActivity.java
+++ client-app/app/src/main/java/com/trpo6/receiptanalyzer/activity/RegActivity.java
@@ -7,19 +7,21 @@ import android.view.View;
 
 import com.trpo6.receiptanalyzer.R;
 import com.trpo6.receiptanalyzer.activity.MainActivity;
-
+/**
+ * Окно регистрации
+ * */
 public class RegActivity extends AppCompatActivity {
-
+    /**Запуск окна регистрации*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg);
     }
+    /**Действия, совершаемые после нажатия на кнопку*/
     public void menuOpen(View view) {
-        // действия, совершаемые после нажатия на кнопку
-        // Создаем объект Intent для вызова новой Activity
+        /**Объект Intent для вызова новой Activity*/
         Intent intent = new Intent(this, MainActivity.class);
-        // запуск activity
+        /**Запуск activity*/
         startActivity(intent);
     }
 }
