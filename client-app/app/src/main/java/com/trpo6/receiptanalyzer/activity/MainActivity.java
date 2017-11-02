e7cc9c50a92293e5faee32e79d05dbf5278cf6b4
Olga <lesscau@gmail.com>
2017-11-03 02:00:23 +0300

    Added test user registration
    by tapping history button

--- client-app/app/src/main/java/com/trpo6/receiptanalyzer/activity/MainActivity.java
+++ client-app/app/src/main/java/com/trpo6/receiptanalyzer/activity/MainActivity.java
@@ -2,9 +2,7 @@ package com.trpo6.receiptanalyzer.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -12,7 +10,8 @@ import android.widget.Button;
 import com.trpo6.receiptanalyzer.R;
 import com.trpo6.receiptanalyzer.api.ApiService;
 import com.trpo6.receiptanalyzer.api.RetroClient;
-import com.trpo6.receiptanalyzer.model.User;
+import com.trpo6.receiptanalyzer.model.RegistrationBody;
+import com.trpo6.receiptanalyzer.model.RegistrationResponse;
 import com.trpo6.receiptanalyzer.utils.InternetConnection;
 
 import retrofit2.Call;
@@ -24,6 +23,52 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        /** Временный код для тестирования соединения с сервером по нажатию кнопки История */
+        Button download = (Button) findViewById(R.id.historyButton);
+        download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /**
+                 * Checking Internet Connection
+                 */
+                if (InternetConnection.checkConnection(getApplicationContext())) {
+                    ApiService api = RetroClient.getApiService();
+                    //User user  = new User("89112356232","pass");
+                    RegistrationBody body = new RegistrationBody("myLogin","+79112734540",590307,"sdfj");
+                    Call<RegistrationResponse> call = api.registerUser(body);
+                    Log.i("i",call.request().toString());
+                    /**
+                     * Enqueue Callback will be call when get response...
+                     */
+                    call.enqueue(new Callback<RegistrationResponse>() {
+                        @Override
+                        public void onResponse(Call<RegistrationResponse> call, Response<RegistrationResponse> response) {
+                            if (response.isSuccessful()) {
+                                /**
+                                 * Got Successfully
+                                 */
+                                //String ans = response.body().toString();
+                                Log.i("success", response.toString());
+                            } else {
+                                Log.e("err0", response.toString());
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<RegistrationResponse> call, Throwable t) {
+                            Log.e("err1", t.toString());
+                        }
+                    });
+
+                } else {
+                    Log.e("error","cant connect");
+                }
+
+            }
+        });
+
     }
 
     /** Переход к активности редактирования списка продуктов */
--- client-app/app/src/main/java/com/trpo6/receiptanalyzer/api/ApiService.java
+++ client-app/app/src/main/java/com/trpo6/receiptanalyzer/api/ApiService.java
@@ -1,17 +1,15 @@
 package com.trpo6.receiptanalyzer.api;
 
 import com.trpo6.receiptanalyzer.model.Item;
-import com.trpo6.receiptanalyzer.model.User;
+import com.trpo6.receiptanalyzer.model.RegistrationBody;
+import com.trpo6.receiptanalyzer.model.RegistrationResponse;
 
 import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.Field;
 import retrofit2.http.GET;
-import retrofit2.http.Header;
 import retrofit2.http.POST;
-import retrofit2.http.Query;
 
 /**
  * Created by User on 27.10.2017.
@@ -21,7 +19,6 @@ public interface ApiService {
     @GET("")
     Call<ArrayList<Item>> products();
 
-    @GET("/login")
-    //Call<String> login(@Field("loginPhone") String loginPhone, @Field("smsPass") String smsPass);
-    Call <User> login (@Query("username") String username, @Query("password") String password);
+    @POST("/api/v1.0/users")
+    Call<RegistrationResponse> registerUser(@Body RegistrationBody registrationBody);
 }
