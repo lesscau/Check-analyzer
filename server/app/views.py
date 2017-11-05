a60ca4437b5e4a32455b5dea1ae4e883d8965969
Zobkov Dmitry <dan0na@ya.ru>
2017-11-05 23:53:56 +0300

    Removed redundant /receipt request from views.py

--- server/app/views.py
+++ server/app/views.py
@@ -1,27 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import request, jsonify
-import json
 from app import app
-from app.models import User
-from .FtsRequest import FtsRequest
 
 # REST API v1 URL pattern
 APIv1 = "/api/v1.0"
-
-# TODO: Rewrite as REST
-# Password from json not used because it doesn't exist in current User model
-@app.route(APIv1 + '/receipt')
-def getReceipt():
-    # Get JSON from GET request
-    json = request.get_json(force = True)
-    # Parse QR-string
-    qrArgs = json['qr'].split("&")
-    fn = qrArgs[2][3:]
-    fd = qrArgs[3][2:]
-    fp = qrArgs[4][3:]
-    # Get phone/pass from fts account
-    # user = User.query.filter_by(username = json['login'], password = json['password']).first_or_404()
-    user = User.query.filter_by(username = json['login']).first_or_404()
-
-    receipt = FtsRequest().getReceipt(fn, fd, fp, user.phone, user.fts_key)
-    return jsonify(receipt)
