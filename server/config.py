533ba199dd18b49147f5b007c84122deedb304cf
Zobkov Dmitry <dan0na@ya.ru>
2017-11-08 03:28:03 +0300

    RA-49: Second try to fix empty os env variables in Dockerfile

--- server/config.py
+++ server/config.py
@@ -1,7 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-if os.environ.get('DATABASE_URL') is None:
+if os.environ.get('DATABASE_URL') is None or os.environ.get('DATABASE_URL') is "":
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
@@ -11,7 +11,7 @@ JSON_AS_ASCII = False
 ERROR_404_HELP = False
 CSRF_ENABLED = True
 
-if os.environ.get('FLASK_APP_SECRET_KEY') is None:
+if os.environ.get('FLASK_APP_SECRET_KEY') is None or os.environ.get('FLASK_APP_SECRET_KEY') is "":
     SECRET_KEY = 'FU8vYvAvQB3q8XVrtPFcjcSbv'
 else:
     SECRET_KEY = os.environ['FLASK_APP_SECRET_KEY']
