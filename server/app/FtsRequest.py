85f5006e03fbe25990fc47783eeb25f2924fe21c
Zobkov Dmitry <dan0na@ya.ru>
2017-11-17 04:53:57 +0300

    Added docstrings to FtsRequest.py

--- server/app/FtsRequest.py
+++ server/app/FtsRequest.py
@@ -3,8 +3,15 @@ import json
 import uuid
 
 
-# TODO: write docstrings
 class FtsRequest:
+    """
+    Requests to Federal Tax Service
+
+    :var     baseUrl:   Base Federal Tax Service URL
+    :vartype baseUrl:   str
+    :ivar    headers:   Request headers
+    :vartype headers:   dict
+    """
     baseUrl = "https://proverkacheka.nalog.ru:9999"
 
     def __init__(self):
@@ -15,10 +22,20 @@ class FtsRequest:
                         'user-agent':    "okhttp/3.0.1"}
 
     def signUp(self, name, email, phone):
+        """
+        Create new user in Federal Tax Service and get password in SMS
+
+        :param name:  Name of user
+        :type  name:  str
+        :param email: User email
+        :type  email: str
+        :param phone: User phone number
+        :type  phone: str
+        :return:      JSON response
+        :rtype:       dict
+        """
         url = "{}/v1/mobile/users/signup".format(self.baseUrl)
-
         data = {"name": name, "email": email, "phone": phone}
-
         response = post(url, headers=self.headers, json=data)
 
         if response.ok:
@@ -30,10 +47,16 @@ class FtsRequest:
             return json.loads(json.dumps(JSON))
 
     def restorePassword(self, phone):
+        """
+        Restore SMS password for existing user in Federal Tax Service
+
+        :param phone: User phone number
+        :type  phone: str
+        :return:      JSON response
+        :rtype:       dict
+        """
         url = "{}/v1/mobile/users/restore".format(self.baseUrl)
-
         data = {"phone": phone}
-
         response = post(url, headers=self.headers, json=data)
 
         if response.ok:
@@ -45,13 +68,38 @@ class FtsRequest:
             return json.loads(json.dumps(JSON))
 
     def checkAuthData(self, loginPhone, smsPass):
+        """
+        Check if user exists in Federal Tax Service
+
+        :param loginPhone: User phone number
+        :type  loginPhone: str
+        :param smsPass:    Password from SMS
+        :type  smsPass:    str/int
+        :return:           True if user exists, False otherwise
+        :rtype:            bool
+        """
         url = "{}/v1/mobile/users/login".format(self.baseUrl)
         auth = (loginPhone, smsPass)
-
         response = get(url, headers=self.headers, auth=auth)
         return response.ok
 
     def getReceipt(self, fn, fd, fp, loginPhone, smsPass):
+        """
+        Get receipt JSON from Federal Tax Service
+
+        :param fn:         ФН number
+        :type  fn:         str
+        :param fd:         ФД number
+        :type  fd:         str
+        :param fp:         ФП number
+        :type  fp:         str
+        :param loginPhone: User phone number
+        :type  loginPhone: str
+        :param smsPass:    Password from SMS
+        :type  smsPass:    str/int
+        :return:           JSON response
+        :rtype:            dict
+        """
         url = "{}/v1/inns/*/kkts/*/fss/{}/tickets/{}" \
               "?fiscalSign={}&sendToEmail=no".format(self.baseUrl, fn, fd, fp)
         auth = (loginPhone, smsPass)
