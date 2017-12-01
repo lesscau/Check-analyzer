8187df5f4d576666ac583bcbd0725f465e0303f2
Zobkov Dmitry <dan0na@ya.ru>
2017-12-01 21:04:57 +0300

    Testing and fixing /tables requests

--- server/app/rest/Tables.py
+++ server/app/rest/Tables.py
@@ -1,6 +1,7 @@
 from flask import g
 from flask_restplus import Namespace, Resource, fields, abort
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import UnmappedInstanceError
 from app import db
 from app.models import User, Table, UserTable
 from app.rest.Auth import Auth
@@ -15,7 +16,7 @@ api = Namespace('Tables', description='Operations with tables', path='/tables')
 # Find table request JSON fields
 table_request = api.parser()
 table_request.add_argument('table_key', type=str, required=True,
-    help='No key provided', location='json')
+    help='No table_key provided', location='json')
 
 # JSON Models #
 
@@ -34,8 +35,8 @@ find_table_request_fields = api.model('UserTable request',
 # TablesUsers response JSON template
 user_table_response_fields = api.model('UserTable request',
 {
-    'table_id': fields.Integer(description='Table_id', required=True),
-    'user_id': fields.Integer(description='User_id')
+    'table_id': fields.Integer(description='Table id', required=True),
+    'user_id': fields.Integer(description='User id')
 })
 
 # TablesUsers list response item
@@ -48,15 +49,17 @@ tables_users_item_fields = api.model('Item TablesUsers response',
 # TablesUsers list response JSON template
 user_table_list_response_fields = api.model('UserTable list request',
 {
-    'items': fields.List(fields.Nested(tables_users_item_fields)),
+    'users': fields.List(fields.Nested(tables_users_item_fields)),
 })
 
 
 @api.route('', endpoint='tables')
 class Tables(Resource):
     """
-    Operations with list of tables
+    Operations with tables
 
+    :var     method_decorators: Decorators applied to methods
+    :vartype method_decorators: list
     """
     method_decorators = [Auth.multi_auth.login_required]
 
@@ -69,7 +72,7 @@ class Tables(Resource):
         """
         Create new table
 
-        :return: New table
+        :return: New table key phrase
         :rtype:  dict/json
         """
 
@@ -82,6 +85,7 @@ class Tables(Resource):
             table_key=key,
             table_date=datetime.utcnow())
 
+        # TODO: Delete try-except or stop recursive key generation after X tries
         try:
             db.session.add(table)
             db.session.flush()
@@ -90,7 +94,6 @@ class Tables(Resource):
             abort(409, message="Table '{}' already exist".format(key))
 
         # Create user-table dependency
-        table = Table.query.filter_by(table_key=key).first()
         user_table = UserTable(
             user_id=user.id,
             table_id=table.id)
@@ -108,6 +111,8 @@ class Tables(Resource):
         """
         Set the first keyword generated does not exist in database
 
+        :return: New key phrase
+        :rtype:  str
         """
         keyword = randomPhrase()
         if Table.query.filter_by(table_key=keyword).first() is not None:
@@ -119,57 +124,68 @@ class Tables(Resource):
 @api.route('/users', endpoint='tables_users')
 class TablesUsers(Resource):
     """
-    Operations with  tables - users dependencies
+    Operations with users in table
 
+    :var     method_decorators: Decorators applied to methods
+    :vartype method_decorators: list
     """
     method_decorators = [Auth.multi_auth.login_required]
 
-    @api.marshal_with(user_table_response_fields)
+    @api.marshal_with(user_table_list_response_fields)
     @api.doc(responses={
-        400: 'User does not exist at any tables'
+        404: 'Username does not connected to any table'
     })
     def get(self):
         """
         Get list of users related to the same table as auth user
 
-        :return: list of users' ids and names
+        :return: list of users id's and names
         :rtype:  dict/json
         """
 
         # Login of authorized user stores in Flask g object
         user = User.query.filter_by(username=g.user.username).first()
 
-        result = {}
-        result['items'] = [{
-            'id': item.user_id,
-            'name': item.user.name
-        } for item in UserTable.query.filter_by(table_id=user.current_table.id)]
-        return result
+        try:
+            result = {}
+            result['users'] = [{
+                'id': item.user_id,
+                'name': item.user.username
+            } for item in user.current_table[0].user_tables]
+            return result
+        except IndexError:
+            abort(404, message="Username '{}' does not connected to any table".format(user.username))
 
     @api.expect(find_table_request_fields)
     @api.marshal_with(user_table_response_fields, code=201)
     @api.doc(responses={
-        400: 'Table keyword does not exist',
-        409: 'User already relates to a table',
+        400: 'No table_key provided',
+        404: 'Table keyword does not exist',
+        406: 'Username already connected with table',
     })
     def post(self):
         """
-        Add new user at the table
+        Add new user to the table
 
-        :return: table-user dependency
+        :return: table and user id's
         :rtype:  dict/json
         """
 
         # Parsing request JSON fields
-        args = user_request.parse_args()
+        args = table_request.parse_args()
 
         # Login of authorized user stores in Flask g object
         user = User.query.filter_by(username=g.user.username).first()
 
+        table = Table.query.filter_by(table_key=args['table_key']).first()
+
+        if table is None:
+            abort(404, message="Table keyword '{}' does not exist".format(args['table_key']))
+
         # Create user-table dependency
         user_table = UserTable(
             user_id=user.id,
-            table_id=user.current_table.id)
+            table_id=table.id)
 
         try:
             db.session.add(user_table)
@@ -178,12 +194,12 @@ class TablesUsers(Resource):
             return user_table, 201
         except IntegrityError:
             db.session.rollback()
-            abort(400, message="Table keyword '{}' does not exist".format(table.table_key))
+            abort(406, message="Username '{}' already connected with table".format(user.username))
 
     @api.marshal_with(user_table_response_fields)
     @api.doc(responses={
-        406: 'User does not exist at any tables',
-        400: 'Table does not exist'
+        400: 'Table does not exist',
+        404: 'Username does not connected to any table',
     })
     def delete(self):
         """
@@ -200,23 +216,17 @@ class TablesUsers(Resource):
 
         try:
             db.session.delete(user_table)
-            db.session.flush()
             # Return JSON using template
-        except IntegrityError:
+        except UnmappedInstanceError:
             db.session.rollback()
-            abort(406, message="User '{}' does not exist".format(user.id))
+            abort(404, message="Username '{}' does not connected to any table".format(user.username))
 
         user_table_list = UserTable.query.filter_by(table_id=user_table.table_id).first()
-        if user_table_list is None:
-            table = Table.query.filter_by(id=user_table.table_id).first()
-            try:
-                db.session.delete(table)
-            except IntegrityError:
-                db.session.rollback()
-                abort(400, message="Table '{}' does not exist".format(table.id))
         try:
+            if user_table_list is None:
+                db.session.delete(user_table.table)
             db.session.commit()
             return user_table
-        except IntegrityError:
+        except (IntegrityError, UnmappedInstanceError):
             db.session.rollback()
-            abort(400, message="Table '{}' does not exist".format(table.id))
+            abort(400, message="Table '{}' does not exist".format(user_table.table.table_key))
