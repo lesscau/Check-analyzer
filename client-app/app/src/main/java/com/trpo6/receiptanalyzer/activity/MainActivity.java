7c055d6b245df5a4e9e9fe6aafa2d10c98e65ceb
Olga <lesscau@gmail.com>
2017-12-01 15:52:56 +0300

    Added toolbar menu
    With Mike Penz MaterialDrawer library: 
    https://github.com/mikepenz/MaterialDrawer

--- client-app/app/src/main/java/com/trpo6/receiptanalyzer/activity/MainActivity.java
+++ client-app/app/src/main/java/com/trpo6/receiptanalyzer/activity/MainActivity.java
@@ -1,14 +1,20 @@
 package com.trpo6.receiptanalyzer.activity;
 
-import android.content.Context;
-import android.content.DialogInterface;
+
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.trpo6.receiptanalyzer.R;
 import com.trpo6.receiptanalyzer.utils.AuthInfo;
 
@@ -17,7 +23,11 @@ import com.trpo6.receiptanalyzer.utils.AuthInfo;
  */
 public class MainActivity extends AppCompatActivity {
 
-    /**Запуск главного меню*/
+    //private FloatingNavigationView mFloatingNavigationView;
+    private Drawer drawerResult = null;
+    /**
+     * Запуск главного меню
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,29 +35,77 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
         setSupportActionBar(mActionBarToolbar);
+        if(getSupportActionBar() != null){
+            Log.i("toolbar","not null");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        else
+            Log.e("Toolbar","is null");
+
+        drawerResult = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(mActionBarToolbar)
+                .withActionBarDrawerToggle(true)
+                .withHeader(R.layout.drawer_header)
+                .addDrawerItems(
+                    new PrimaryDrawerItem()
+                        .withName(R.string.toMain)
+                        .withIcon(FontAwesome.Icon.faw_home)
+                        .withIdentifier(1),
+                    new PrimaryDrawerItem()
+                        .withName(R.string.history)
+                        .withIcon(FontAwesome.Icon.faw_history)
+                        .withIdentifier(2),
+                    new DividerDrawerItem(),
+                    new SecondaryDrawerItem()
+                        .withName(R.string.logout)
+                        .withIcon(FontAwesome.Icon.faw_sign_out)
+                        .withIdentifier(3)
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener(){
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        if (drawerItem.getIdentifier()==3){
+                            logOut(view);
+                            return true;
+                        }
+                        return false;
+                    }
+
+                })
+                .build();
+
         // проверяем, была ли ранее выполнена авторизация
-        if (!AuthInfo.isAuthorized(this)){
+        if (!AuthInfo.isAuthorized(this)) {
             // Окно логина/регистрации
             Intent intent = new Intent(this, FirstActivity.class);
 
             // запуск activity
             startActivity(intent);
         }
+
     }
 
-    /** Переход к активности редактирования списка продуктов */
-    public void addItemsWindow(View view){
+    /**
+     * Переход к активности редактирования списка продуктов
+     */
+    public void addItemsWindow(View view) {
         Intent intent = new Intent("productlist");
         startActivity(intent);
     }
 
-    /** Переход к активности сканирования QR-кода с чека */
-    public void scanQRcode(View view){
+    /**
+     * Переход к активности сканирования QR-кода с чека
+     */
+    public void scanQRcode(View view) {
         Intent intent = new Intent("QRscanner");
         startActivity(intent);
     }
-    /** Переход к первой активити, если нажали LogOut */
-    public void logOut(View view){
+
+    /**
+     * Переход к первой активити, если нажали LogOut
+     */
+    public void logOut(View view) {
         // стираем старый токен
         AuthInfo.authClear(this);
 
@@ -57,7 +115,14 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    /** Игнорирование нажатия кнопки "назад" */
+    /**
+     * Игнорирование нажатия кнопки "назад"
+     */
     @Override
-    public void onBackPressed() {}
+    public void onBackPressed() {
+        if(drawerResult.isDrawerOpen()){
+            drawerResult.closeDrawer();
+        }
+        else{}
+    }
 }
